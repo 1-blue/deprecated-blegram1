b05e91f570cfbdd8e4b41e7f2e01c0fc7fcbf978
1-blue <1-blue98@naver.com>
2023-04-09 22:25:04 +0900

    Feat: (BE) Add fetching posts logic
    게시글들 가져오는 엔드포인트 추가

---
+++ src/pages/api/posts.ts
@@ -0,0 +1,72 @@
+// prisma
+import { prisma } from "@src/prisma";
+
+// lib
+import withAuthMiddleware from "@src/lib/middleware";
+
+// type
+import type { NextApiHandler } from "next";
+import type { ApiFetchPostsResponse } from "@src/types/api";
+
+/** 2023/04/08 - 게시글들 관련 엔드포인트 - by 1-blue */
+const handler: NextApiHandler<ApiFetchPostsResponse> = async (req, res) => {
+  try {
+    // 모든 게시글들 가져오기 요청
+    if (req.method === "GET") {
+      const take = +(req.query.take as string);
+      const lastIdx = +(req.query.lastIdx as string);
+
+      const posts = await prisma.post.findMany({
+        where: {},
+        take: take,
+        skip: lastIdx === -1 ? 0 : 1,
+        ...(lastIdx !== -1 && { cursor: { idx: lastIdx } }),
+        orderBy: { updatedAt: "desc" },
+        include: {
+          user: {
+            select: {
+              idx: true,
+              avatar: true,
+              nickname: true,
+            },
+          },
+          comments: {},
+          postLiker: {
+            select: {
+              postLiker: {
+                select: {
+                  idx: true,
+                  avatar: true,
+                  nickname: true,
+                },
+              },
+            },
+          },
+          _count: {
+            select: {
+              comments: true,
+              postLiker: true,
+            },
+          },
+        },
+      });
+
+      return res.status(200).json({
+        message: `최신 게시글 ${take}개를 가져왔습니다.`,
+        posts,
+      });
+    }
+  } catch (error) {
+    console.error("/api/user error >> ", error);
+
+    return res.status(500).json({
+      message: "서버측 문제입니다.\n잠시후에 다시 시도해주세요!",
+    });
+  }
+};
+
+export default withAuthMiddleware({
+  methods: ["GET"],
+  handler,
+  isAuth: false,
+});
